1da408191d69b5dcc605c0cff2214f3059430d4c
Julian <julianaguero@gmail.com>
2024-06-24 21:17:52 +0200

    chore: create index.ts to export hooks efficiently

---
+++ src/hooks/index.ts
@@ -0,0 +1,11 @@
+import useCartContext from "./useCartContext";
+import useSearch from "./useSearch";
+import { useThemeContext } from "./useThemeContext";
+import useToggleNavbar from "./useToggleNabar";
+
+export {
+    useCartContext, 
+    useSearch, 
+    useThemeContext,
+    useToggleNavbar
+}
