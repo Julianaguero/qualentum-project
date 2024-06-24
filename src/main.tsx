747b0cd43397e2df91be2b087bd28dd061b19448
Julian <julianaguero@gmail.com>
2024-06-24 23:16:46 +0200

    feat: add CardContextProvider component wrapping App

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import ThemeContextProvider from "./context/ThemeContext.tsx";
 import "./index.css";
+import CartContextProvider from "./context/CartContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeContextProvider>
-      <App />
+      <CartContextProvider>
+        <App />
+      </CartContextProvider>
     </ThemeContextProvider>
   </React.StrictMode>
 );
