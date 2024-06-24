d933b2026f56e75bd1be029e1337e6d0b8d849a4
Julian <julianaguero@gmail.com>
2024-06-24 13:26:24 +0200

    feat: create CustomButton.tsx, add to ProductCard.tsx, and add CustomButton.css

---
+++ src/components/Buttons/CustomButton.tsx
@@ -0,0 +1,28 @@
+import "./CustomButton.css"
+
+interface CustomButtonProps extends React.HTMLAttributes<HTMLButtonElement>{
+    text: string,
+    className?: string,
+    disabled?: boolean,
+    action: () => void,
+}
+
+const CustomButton : React.FC<CustomButtonProps> = 
+
+({text,  className, disabled, action, ...props}) => {
+ 
+    return (
+        <button
+            {...props}
+            className={`custom-button ${className}`}
+            disabled={disabled}
+            onClick={action}
+        >
+            {text}
+        </button>
+    )
+}
+
+
+
+export default CustomButton
