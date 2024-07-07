80c016244a61650b5760e340f8a14a14dee8a2cf
Julian <julianaguero@gmail.com>
2024-07-07 22:06:19 +0200

    fix: change router path

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import CartContextProvider from "./context/CartContext.tsx";
 import UserContextProvider from "./context/AuthContext.tsx";
 import ProductsContextProvider  from "./context/ProductsContext.tsx";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./Routes/index.tsx";
+import { router } from "./routes/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
