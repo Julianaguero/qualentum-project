1cc1be2d791b0947c61d424af53406c1ffc2715c
Julian <julianaguero@gmail.com>
2024-07-23 10:23:42 +0200

    fix: rename folder to fix case insensitive issue

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import ProductsContextProvider from "./context/unusedContext/ProductsContext.tsx";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/index.tsx";
+import { router } from "./otronombre/index.tsx";
 import { Provider } from "react-redux";
 import { store } from "./state/store.ts";
 
